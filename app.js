55bac16dc0eaa756eaa4da3656fc97a046b87e2d
kameron <kamscott3@gmail.com>
2022-04-15 15:18:30 -0400

    hw

---
+++ app.js
@@ -0,0 +1,42 @@
+var a = 144;
+
+var b = 12;
+
+function addSum(param1, param2){
+    return param1 + param2;
+}
+console.log(a, b);
+
+let addSum = (a, b) => a + b;
+
+
+
+function addSum(param1, param2){
+    return param1 - param2;
+}
+console.log(a, b);
+
+let addSum = (a, b) => a - b;
+
+
+
+function addSum(param1, param2){
+    return param1 * param2;
+}
+console.log(a, b);
+
+let addSum = (a, b) => a * b;
+
+
+
+function addSum(param1, param2){
+    return param1 / param2;
+}
+console.log(a, b);
+
+let addSum = (a, b) => a / b;
+    
+
+
+
+
